967fa8a0c72cb066f698632d55967e28535c93da
DURESSAH <daregotmedia4@gmail.com>
2026-03-19 16:08:40 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-// You can add animations later
-console.log("TGAH Website Ready");
+// SCROLL ANIMATION
+const elements = document.querySelectorAll(".fade");
+
+window.addEventListener("scroll", () => {
+    elements.forEach(el => {
+        if (el.getBoundingClientRect().top < window.innerHeight - 100) {
+            el.classList.add("show");
+        }
+    });
+});
+
+// IMAGE MODAL
+function openModal(src) {
+    document.getElementById("modal").style.display = "flex";
+    document.getElementById("modal-img").src = src;
+}
+
+function closeModal() {
+    document.getElementById("modal").style.display = "none";
+}
